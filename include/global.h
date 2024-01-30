63e34261ceb9ff872f56ac121d83f1613eb5fd5c
nescioquid <info@creachr.com>
2024-01-30 11:42:37 -0600

    ghoulslash's register-LR feature

--- include/global.h
+++ include/global.h
@@ -991,7 +991,7 @@ struct SaveBlock1
     /*0x238*/ struct Pokemon playerParty[PARTY_SIZE];
     /*0x490*/ u32 money;
     /*0x494*/ u16 coins;
-    /*0x496*/ u16 registeredItem; // registered for use with SELECT button
+    /*0x496*/ u16 registeredItemSelect;
     /*0x498*/ struct ItemSlot pcItems[PC_ITEMS_COUNT];
     /*0x560*/ struct ItemSlot bagPocket_Items[BAG_ITEMS_COUNT];
     /*0x5D8*/ struct ItemSlot bagPocket_KeyItems[BAG_KEYITEMS_COUNT];
@@ -1082,6 +1082,8 @@ struct SaveBlock1
     /*0x3???*/ struct TrainerHillSave trainerHill;
 #endif //FREE_TRAINER_HILL
     /*0x3???*/ struct WaldaPhrase waldaPhrase;
+    /*0x3D88*/ u16 registeredItemL;
+    /*0x3D8A*/ u16 registeredItemR;
     // sizeof: 0x3???
 };
 
