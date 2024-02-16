693ef66a6393d62d2c52d206525b994d5181c414
nescioquid <info@creachr.com>
2024-02-16 14:14:11 -0600

    adds Medicine and Held Items pockets
    adapted from Eduardo's BetterBag_ItemExpansion feature

--- src/strings.c
+++ src/strings.c
@@ -203,7 +203,9 @@ const u8 *const gPocketNamesStringsTable[] =
     [POCKET_POKE_BALLS] =   COMPOUND_STRING("Poké Balls"),
     [POCKET_TM_HM]  =       COMPOUND_STRING("TMs & HMs"),
     [POCKET_BERRIES] =      COMPOUND_STRING("Berries"),
-    [POCKET_KEY_ITEMS] =    COMPOUND_STRING("Key Items")
+    [POCKET_KEY_ITEMS] =    COMPOUND_STRING("Key Items"),
+    [POCKET_MEDICINE] =     COMPOUND_STRING("MEDICINE"),
+    [POCKET_HELD_ITEMS] =   COMPOUND_STRING("HELD ITEMS")
 };
 
 const u8 gText_NumberItem_TMBerry[] = _("{NO}{STR_VAR_1}{CLEAR 0x07}{STR_VAR_2}");
@@ -499,6 +501,8 @@ const u8 gText_Key_Items[] = _("Key Items");
 const u8 gText_Poke_Balls[] = _("Poké Balls");
 const u8 gText_TMs_Hms[] = _("TMs & HMs");
 const u8 gText_Berries2[] = _("Berries");
+const u8 gText_Medicine[] = _("MEDICINE");
+const u8 gText_HeldItems[] = _("HELD ITEMS");
 const u8 gText_SomeonesPC[] = _("Someone's PC");
 const u8 gText_LanettesPC[] = _("Lanette's PC");
 const u8 gText_PlayersPC[] = _("{PLAYER}'s PC");
