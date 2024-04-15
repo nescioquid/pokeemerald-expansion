06d7a89ff6b8be8e62a38b35a2571d28bb4d9ccb
nescioquid <info@creachr.com>
2024-04-15 15:24:40 -0600

    adds randomization for item balls

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1086,3 +1086,4 @@ EventScript_VsSeekerChargingDone::
 .include "data/scripts/pory/items/tms_hms.inc"
 .include "data/scripts/pory/items/tms_hms_marts.inc"
 .include "data/scripts/pory/items/randomized_item_hidden.inc"
+.include "data/scripts/pory/items/randomized_item_ball.inc"
