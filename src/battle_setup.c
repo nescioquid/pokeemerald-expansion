d7a9e507d6c96f946833dd7e0e19f335608c031a
nescioquid <info@creachr.com>
2024-04-16 19:12:13 -0600

    changes and buffs the starter selections
    - gen 1 stage 2 mons
    - level 25
    - hold Eviolites

--- src/battle_setup.c
+++ src/battle_setup.c
@@ -872,7 +872,7 @@ static void CB2_GiveStarter(void)
 
     *GetVarPointer(VAR_STARTER_MON) = gSpecialVar_Result;
     starterMon = GetStarterPokemon(gSpecialVar_Result);
-    ScriptGiveMon(starterMon, 5, ITEM_NONE);
+    ScriptGiveMon(starterMon, 25, ITEM_EVIOLITE);
     ResetTasks();
     PlayBattleBGM();
     SetMainCallback2(CB2_StartFirstBattle);
