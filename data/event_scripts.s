92019d0486b92a18eaf11eb3c210026cdb14ad9f
nescioquid <info@creachr.com>
2024-04-16 23:09:57 -0600

    converts the Diving Treasure Hunter into a Pearl maniac who also gives HM08 Dive

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1079,3 +1079,4 @@ EventScript_VsSeekerChargingDone::
 .include "data/scripts/pory/items/mr_stone.inc"
 .include "data/scripts/pory/items/steven_mossdeep.inc"
 .include "data/scripts/pory/items/gym_guide.inc"
+.include "data/scripts/pory/items/diving_treasure_hunter.inc"
