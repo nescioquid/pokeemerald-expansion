3fa56b62569d5ba4e6c3a95767e0b0678afd4ba2
nescioquid <info@creachr.com>
2024-04-18 14:19:46 -0600

    makes the Devon Employee give a Net Ball, an Exp. Share, and a Master Ball

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1075,3 +1075,4 @@ EventScript_VsSeekerChargingDone::
 .include "data/scripts/pory/items/dewford_silk_scarf.inc"
 .include "data/scripts/pory/items/granite_cave_hiker.inc"
 .include "data/scripts/pory/items/winstrates.inc"
+.include "data/scripts/pory/items/devon_employee.inc"
