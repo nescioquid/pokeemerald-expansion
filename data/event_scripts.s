397f9deec58fe172d099596266a5f2b0f7e82161
nescioquid <info@creachr.com>
2024-04-18 14:30:09 -0600

    changes some dialogues in the early game

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1073,3 +1073,4 @@ EventScript_VsSeekerChargingDone::
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
 .include "data/scripts/pory/gt/overwold_mons.inc"
+.include "data/scripts/pory/gt/npc_dialogues.inc"
