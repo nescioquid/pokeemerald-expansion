0581c216f5d98beb0bed6e27ba4cbec1373eed78
nescioquid <info@creachr.com>
2024-04-19 19:49:41 -0600

    makes the Seashore House owner now sell Soda Pops by the six pack

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1077,3 +1077,4 @@ EventScript_VsSeekerChargingDone::
 .include "data/scripts/pory/quality_of_life/match_call_tutorial.inc"
 .include "data/scripts/pory/quality_of_life/devon_parts.inc"
 .include "data/scripts/pory/quality_of_life/florist_heal_mons.inc"
+.include "data/scripts/pory/quality_of_life/seashore_house.inc"
