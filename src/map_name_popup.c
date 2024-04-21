0bd23b0ab05336f364ffce827f5a0eead6ec0cd2
nescioquid <info@creachr.com>
2024-04-21 10:32:24 -0600

    splits Route 104 into Route 104 North and Route 104 South

--- src/map_name_popup.c
+++ src/map_name_popup.c
@@ -102,6 +102,8 @@ static const u8 sRegionMapSectionId_To_PopUpThemeIdMapping[] =
     [MAPSEC_ROUTE_102] = MAPPOPUP_THEME_WOOD,
     [MAPSEC_ROUTE_103] = MAPPOPUP_THEME_WOOD,
     [MAPSEC_ROUTE_104] = MAPPOPUP_THEME_WOOD,
+    [MAPSEC_ROUTE_104_NORTH] = MAPPOPUP_THEME_WOOD,
+    [MAPSEC_ROUTE_104_SOUTH] = MAPPOPUP_THEME_WOOD,
     [MAPSEC_ROUTE_105] = MAPPOPUP_THEME_UNDERWATER,
     [MAPSEC_ROUTE_106] = MAPPOPUP_THEME_UNDERWATER,
     [MAPSEC_ROUTE_107] = MAPPOPUP_THEME_UNDERWATER,
@@ -611,13 +613,13 @@ static void LoadMapNamePopUpWindowBg(void)
     if (OW_POPUP_GENERATION == GEN_5)
         secondaryPopUpWindowId = GetSecondaryPopUpWindowId();
 
-    if (regionMapSectionId >= KANTO_MAPSEC_START)
-    {
-        if (regionMapSectionId > KANTO_MAPSEC_END)
-            regionMapSectionId -= KANTO_MAPSEC_COUNT;
-        else
-            regionMapSectionId = 0; // Discard kanto region sections;
-    }
+    // if (regionMapSectionId >= KANTO_MAPSEC_START)
+    // {
+    //     if (regionMapSectionId > KANTO_MAPSEC_END)
+    //         regionMapSectionId -= KANTO_MAPSEC_COUNT;
+    //     else
+    //         regionMapSectionId = 0; // Discard kanto region sections;
+    // }
 
     if (OW_POPUP_GENERATION == GEN_5)
     {
