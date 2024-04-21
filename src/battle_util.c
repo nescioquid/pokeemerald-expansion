915be5dfe4ec94b8ab7b1d4169f964ba82881cdc
nescioquid <info@creachr.com>
2024-04-21 14:46:23 -0600

    modifies obedience restrictions and increases the levels of Kyogre, Groudon and Rayquaza by 5

--- src/battle_util.c
+++ src/battle_util.c
@@ -7383,25 +7383,27 @@ u8 GetAttackerObedienceForAction()
         return OBEYS;
     if (B_OBEDIENCE_MECHANICS < GEN_8 && !IsOtherTrainer(gBattleMons[gBattlerAttacker].otId, gBattleMons[gBattlerAttacker].otName))
         return OBEYS;
-    if (FlagGet(FLAG_BADGE08_GET)) // Rain Badge, ignore obedience altogether
+    if (FlagGet(FLAG_SYS_GAME_CLEAR)) // ignore obedience altogether
         return OBEYS;
 
-    obedienceLevel = 10;
-
-    if (FlagGet(FLAG_BADGE01_GET)) // Stone Badge
-        obedienceLevel = 20;
-    if (FlagGet(FLAG_BADGE02_GET)) // Knuckle Badge
         obedienceLevel = 30;
-    if (FlagGet(FLAG_BADGE03_GET)) // Dynamo Badge
-        obedienceLevel = 40;
-    if (FlagGet(FLAG_BADGE04_GET)) // Heat Badge
-        obedienceLevel = 50;
-    if (FlagGet(FLAG_BADGE05_GET)) // Balance Badge
-        obedienceLevel = 60;
-    if (FlagGet(FLAG_BADGE06_GET)) // Feather Badge
-        obedienceLevel = 70;
-    if (FlagGet(FLAG_BADGE07_GET)) // Mind Badge
-        obedienceLevel = 80;
+
+        if (FlagGet(FLAG_BADGE01_GET)) // Stone Badge
+            obedienceLevel = 35;
+        if (FlagGet(FLAG_BADGE02_GET)) // Knuckle Badge
+            obedienceLevel = 40;
+        if (FlagGet(FLAG_BADGE03_GET)) // Dynamo Badge
+            obedienceLevel = 45;
+        if (FlagGet(FLAG_BADGE04_GET)) // Heat Badge
+            obedienceLevel = 45;
+        if (FlagGet(FLAG_BADGE05_GET)) // Balance Badge
+            obedienceLevel = 50;
+        if (FlagGet(FLAG_BADGE06_GET)) // Feather Badge
+            obedienceLevel = 60;
+        if (FlagGet(FLAG_BADGE07_GET)) // Mind Badge
+            obedienceLevel = 65;
+        if (FlagGet(FLAG_BADGE08_GET)) // Rain Badge
+            obedienceLevel = 70;
 
     if (B_OBEDIENCE_MECHANICS >= GEN_8
      && !IsOtherTrainer(gBattleMons[gBattlerAttacker].otId, gBattleMons[gBattlerAttacker].otName))
