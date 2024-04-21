22da201f6cb049d3dde3c02c466f07b360698730
nescioquid <info@creachr.com>
2024-04-21 14:08:59 -0600

    puts limits on heal mon locations

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1079,3 +1079,4 @@ EventScript_VsSeekerChargingDone::
 .include "data/scripts/pory/quality_of_life/florist_heal_mons.inc"
 .include "data/scripts/pory/quality_of_life/seashore_house.inc"
 .include "data/scripts/pory/quality_of_life/nurse_joy.inc"
+.include "data/scripts/pory/quality_of_life/rest_stop.inc"
