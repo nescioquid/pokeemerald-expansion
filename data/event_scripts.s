a61c2fb44952b917e204c7fbc8c6412942a845e0
nescioquid <info@creachr.com>
2024-04-22 10:50:41 -0600

    edits the Running Shoes manual to account for the new functionality

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1112,5 +1112,6 @@ EventScript_VsSeekerChargingDone::
 .include "data/scripts/pory/gt/capt_stern.inc"
 .include "data/scripts/pory/gt/meta_boy.inc"
 .include "data/scripts/pory/gt/champion.inc"
+.include "data/scripts/pory/quality_of_life/running_shoes.inc"
 .include "data/maps/Route104North/scripts.inc"
 .include "data/maps/Route104South/scripts.inc"
