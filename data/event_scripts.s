3272e0abc655495f0f2d1200ef8c9ea331a66673
nescioquid <info@creachr.com>
2024-04-24 19:23:34 -0600

    makes Rival #2 only at Mr. Briney's House and obligatory when first encountered

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1112,6 +1112,7 @@ EventScript_VsSeekerChargingDone::
 .include "data/scripts/pory/gt/capt_stern.inc"
 .include "data/scripts/pory/gt/meta_boy.inc"
 .include "data/scripts/pory/gt/champion.inc"
+.include "data/scripts/pory/gt/rival_route_104.inc"
 .include "data/scripts/pory/quality_of_life/running_shoes.inc"
 .include "data/maps/Route104North/scripts.inc"
 .include "data/maps/Route104South/scripts.inc"
