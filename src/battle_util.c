10bd5fb4c6e931cf4cbd0291c6a793a6a7d798b6
nescioquid <info@creachr.com>
2024-06-02 23:07:02 -0600

    readds obedience restrictions

--- src/battle_util.c
+++ src/battle_util.c
@@ -8055,25 +8055,27 @@ u8 IsMonDisobedient(void)
             return 0;
         if (B_OBEDIENCE_MECHANICS < GEN_8 && !IsOtherTrainer(gBattleMons[gBattlerAttacker].otId, gBattleMons[gBattlerAttacker].otName))
             return 0;
-        if (FlagGet(FLAG_BADGE08_GET)) // Rain Badge, ignore obedience altogether
+        if (FlagGet(FLAG_IS_CHAMPION)) // ignore obedience altogether
             return 0;
 
-        obedienceLevel = 100;
+        obedienceLevel = 30;
 
-        // if (FlagGet(FLAG_BADGE01_GET)) // Stone Badge
-        //     obedienceLevel = 20;
-        // if (FlagGet(FLAG_BADGE02_GET)) // Knuckle Badge
-        //     obedienceLevel = 30;
-        // if (FlagGet(FLAG_BADGE03_GET)) // Dynamo Badge
-        //     obedienceLevel = 40;
-        // if (FlagGet(FLAG_BADGE04_GET)) // Heat Badge
-        //     obedienceLevel = 50;
-        // if (FlagGet(FLAG_BADGE05_GET)) // Balance Badge
-        //     obedienceLevel = 60;
-        // if (FlagGet(FLAG_BADGE06_GET)) // Feather Badge
-        //     obedienceLevel = 70;
-        // if (FlagGet(FLAG_BADGE07_GET)) // Mind Badge
-        //     obedienceLevel = 80;
+        if (FlagGet(FLAG_BADGE01_GET)) // Stone Badge
+            obedienceLevel = 35;
+        if (FlagGet(FLAG_BADGE02_GET)) // Knuckle Badge
+            obedienceLevel = 40;
+        if (FlagGet(FLAG_BADGE03_GET)) // Dynamo Badge
+            obedienceLevel = 45;
+        if (FlagGet(FLAG_BADGE04_GET)) // Heat Badge
+            obedienceLevel = 45;
+        if (FlagGet(FLAG_BADGE05_GET)) // Balance Badge
+            obedienceLevel = 50;
+        if (FlagGet(FLAG_BADGE06_GET)) // Feather Badge
+            obedienceLevel = 60;
+        if (FlagGet(FLAG_BADGE07_GET)) // Mind Badge
+            obedienceLevel = 65;
+        if (FlagGet(FLAG_BADGE08_GET)) // Rain Badge
+            obedienceLevel = 70;
     }
 
     if (B_OBEDIENCE_MECHANICS >= GEN_8
