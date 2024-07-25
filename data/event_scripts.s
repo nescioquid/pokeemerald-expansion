0d5e8ec7a6e3d50aeb920af12af1568b15865a90
nescioquid <info@creachr.com>
2024-07-24 18:14:43 -0600

    makes the Old Rod NPC give a Super Rod, and the Good Rod and Super Rod NPCs give Rare Candies

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1102,6 +1102,7 @@ EventScript_VsSeekerChargingDone::
 .include "data/scripts/pory/items/items_overwold.inc"
 .include "data/scripts/pory/items/mom.inc"
 .include "data/scripts/pory/items/sun_stone_man.inc"
+.include "data/scripts/pory/items/fishing_rods.inc"
 .include "data/scripts/pory/gt/overwold_mons.inc"
 .include "data/scripts/pory/gt/npc_dialogues.inc"
 .include "data/scripts/pory/gt/trade.inc"
