e22ab06678230dc967ca7be197e2594f746e0c17
nescioquid <info@creachr.com>
2024-08-15 03:31:24 -0600

    makes the White Herb NPC now heal your mons

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -120,7 +120,7 @@
 #define FLAG_MOSSDEEP_GYM_SWITCH_3           0x66 //
 #define FLAG_MOSSDEEP_GYM_SWITCH_4           0x67 //
 
-#define FLAG_UNUSED_0x068                    0x68  // Unused Flag
+#define FLAG_MET_WHITE_HERB_FLORIST          0x68
 
 #define FLAG_OCEANIC_MUSEUM_MET_REPORTER     0x69
 #define FLAG_RECEIVED_HM_STRENGTH            0x6A
