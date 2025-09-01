59904607da7e3b6e0d4ebf15399f2260a08eb582
Tyler <tylerjklein24@gmail.com>
2025-08-31 22:49:09 -0400

    Expanded PC storage to 17 boxes

--- include/save.h
+++ include/save.h
@@ -18,14 +18,14 @@
 #define SECTOR_ID_SAVEBLOCK1_START    1
 #define SECTOR_ID_SAVEBLOCK1_END      4
 #define SECTOR_ID_PKMN_STORAGE_START  5
-#define SECTOR_ID_PKMN_STORAGE_END   14 //changed from 13
-#define NUM_SECTORS_PER_SLOT         15 //changed from 14
+#define SECTOR_ID_PKMN_STORAGE_END   15 //changed from 13
+#define NUM_SECTORS_PER_SLOT         16 //changed from 14
 // Save Slot 1: 0-13;  Save Slot 2: 14-27
-#define SECTOR_ID_HOF_1              30 //changed from 28
-#define SECTOR_ID_HOF_2              31 //changed from 29
-#define SECTOR_ID_TRAINER_HILL       32 //changed from 30
-#define SECTOR_ID_RECORDED_BATTLE    33 //changed from 31
-#define SECTORS_COUNT                34 //changed from 32
+#define SECTOR_ID_HOF_1              32 //changed from 28
+#define SECTOR_ID_HOF_2              33 //changed from 29
+#define SECTOR_ID_TRAINER_HILL       34 //changed from 30
+#define SECTOR_ID_RECORDED_BATTLE    35 //changed from 31
+#define SECTORS_COUNT                36 //changed from 32
 
 #define NUM_HOF_SECTORS 2
 
--- src/save.c
+++ src/save.c
@@ -74,7 +74,8 @@ struct
     SAVEBLOCK_CHUNK(struct PokemonStorage, 6),
     SAVEBLOCK_CHUNK(struct PokemonStorage, 7),
     SAVEBLOCK_CHUNK(struct PokemonStorage, 8), //Added new after here
-    SAVEBLOCK_CHUNK(struct PokemonStorage, 9), // SECTOR_ID_PKMN_STORAGE_END
+    SAVEBLOCK_CHUNK(struct PokemonStorage, 9),
+    SAVEBLOCK_CHUNK(struct PokemonStorage, 10), // SECTOR_ID_PKMN_STORAGE_END
 };
 
 // These will produce an error if a save struct is larger than the space
