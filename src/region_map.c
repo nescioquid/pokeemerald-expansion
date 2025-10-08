nescioquid/pokeemerald

splits Route 104 into Route 104 North and Route 104 South

src/region_map.c
@@ -149,7 +149,7 @@ static const u16 sRegionMap_SpecialPlaceLocations[][2] =
     {MAPSEC_AQUA_HIDEOUT_OLD,           MAPSEC_LILYCOVE_CITY},
     {MAPSEC_MAGMA_HIDEOUT,              MAPSEC_ROUTE_112},
     {MAPSEC_UNDERWATER_SEALED_CHAMBER,  MAPSEC_ROUTE_134},
-    {MAPSEC_PETALBURG_WOODS,            MAPSEC_ROUTE_104},
+    {MAPSEC_PETALBURG_WOODS,            MAPSEC_ROUTE_104_SOUTH},
     {MAPSEC_JAGGED_PASS,                MAPSEC_ROUTE_112},
     {MAPSEC_MT_PYRE,                    MAPSEC_ROUTE_122},
     {MAPSEC_SKY_PILLAR,                 MAPSEC_ROUTE_131},
@@ -308,6 +308,8 @@ static const u8 sMapHealLocations[][3] =
     [MAPSEC_ROUTE_102] = {MAP_GROUP(ROUTE102), MAP_NUM(ROUTE102), HEAL_LOCATION_NONE},
     [MAPSEC_ROUTE_103] = {MAP_GROUP(ROUTE103), MAP_NUM(ROUTE103), HEAL_LOCATION_NONE},
     [MAPSEC_ROUTE_104] = {MAP_GROUP(ROUTE104), MAP_NUM(ROUTE104), HEAL_LOCATION_NONE},
+    [MAPSEC_ROUTE_104_NORTH] = {MAP_GROUP(ROUTE104NORTH), MAP_NUM(ROUTE104NORTH), HEAL_LOCATION_NONE},
+    [MAPSEC_ROUTE_104_SOUTH] = {MAP_GROUP(ROUTE104SOUTH), MAP_NUM(ROUTE104SOUTH), HEAL_LOCATION_NONE},
     [MAPSEC_ROUTE_105] = {MAP_GROUP(ROUTE105), MAP_NUM(ROUTE105), HEAL_LOCATION_NONE},
     [MAPSEC_ROUTE_106] = {MAP_GROUP(ROUTE106), MAP_NUM(ROUTE106), HEAL_LOCATION_NONE},
     [MAPSEC_ROUTE_107] = {MAP_GROUP(ROUTE107), MAP_NUM(ROUTE107), HEAL_LOCATION_NONE},

nescioquid/pokeemerald-expansion

src/region_map.c
@@ -149,7 +149,7 @@ static const u16 sRegionMap_SpecialPlaceLocations[][2] =
     {MAPSEC_AQUA_HIDEOUT_OLD,           MAPSEC_LILYCOVE_CITY},
     {MAPSEC_MAGMA_HIDEOUT,              MAPSEC_ROUTE_112},
     {MAPSEC_UNDERWATER_SEALED_CHAMBER,  MAPSEC_ROUTE_134},
-    {MAPSEC_PETALBURG_WOODS,            MAPSEC_ROUTE_104},
+    {MAPSEC_PETALBURG_WOODS,            MAPSEC_ROUTE_104_SOUTH},
     {MAPSEC_JAGGED_PASS,                MAPSEC_ROUTE_112},
     {MAPSEC_MT_PYRE,                    MAPSEC_ROUTE_122},
     {MAPSEC_SKY_PILLAR,                 MAPSEC_ROUTE_131},
@@ -308,6 +308,8 @@ static const u8 sMapHealLocations[][3] =
     [MAPSEC_ROUTE_102] = {MAP_GROUP(MAP_ROUTE102), MAP_NUM(MAP_ROUTE102), HEAL_LOCATION_NONE},
     [MAPSEC_ROUTE_103] = {MAP_GROUP(MAP_ROUTE103), MAP_NUM(MAP_ROUTE103), HEAL_LOCATION_NONE},
     [MAPSEC_ROUTE_104] = {MAP_GROUP(MAP_ROUTE104), MAP_NUM(MAP_ROUTE104), HEAL_LOCATION_NONE},
+    [MAPSEC_ROUTE_104_NORTH] = {MAP_GROUP(MAP_ROUTE104NORTH), MAP_NUM(MAP_ROUTE104NORTH), HEAL_LOCATION_NONE},
+    [MAPSEC_ROUTE_104_SOUTH] = {MAP_GROUP(MAP_ROUTE104SOUTH), MAP_NUM(MAP_ROUTE104SOUTH), HEAL_LOCATION_NONE},
     [MAPSEC_ROUTE_105] = {MAP_GROUP(MAP_ROUTE105), MAP_NUM(MAP_ROUTE105), HEAL_LOCATION_NONE},
     [MAPSEC_ROUTE_106] = {MAP_GROUP(MAP_ROUTE106), MAP_NUM(MAP_ROUTE106), HEAL_LOCATION_NONE},
     [MAPSEC_ROUTE_107] = {MAP_GROUP(MAP_ROUTE107), MAP_NUM(MAP_ROUTE107), HEAL_LOCATION_NONE},