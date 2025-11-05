b43264cc201772df926d7b6d38b46f8ef0440864
Tyler <tylerjklein24@gmail.com>
2025-11-05 01:29:48 -0500

    Updated the .c file and macros so HyperTrain and CanHyperTrain functions can take variables

--- src/script_pokemon_util.c
+++ src/script_pokemon_util.c
@@ -238,7 +238,7 @@ void ReducePlayerPartyToSelectedMons(void)
 
 void CanHyperTrain(struct ScriptContext *ctx)
 {
-    u32 stat = ScriptReadByte(ctx);
+    u32 stat = VarGet(ScriptReadHalfword(ctx));
     u32 partyIndex = VarGet(ScriptReadHalfword(ctx));
 
     Script_RequestEffects(SCREFF_V1);
@@ -258,7 +258,7 @@ void CanHyperTrain(struct ScriptContext *ctx)
 
 void HyperTrain(struct ScriptContext *ctx)
 {
-    u32 stat = ScriptReadByte(ctx);
+    u32 stat = VarGet(ScriptReadHalfword(ctx));
     u32 partyIndex = VarGet(ScriptReadHalfword(ctx));
 
     Script_RequestEffects(SCREFF_V1 | SCREFF_SAVE);
