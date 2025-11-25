2f4d3b9935feb3ad95ebd8a45a8b2d16cb44c8fa
Tyler <tylerjklein24@gmail.com>
2025-11-25 12:07:17 -0500

    Splits Route 104 into Route 104 North and Route 104 South

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1051,7 +1051,7 @@
 // Item Ball Flags
 #define FLAG_ITEM_ROUTE_102_POTION                                  0x3E8
 #define FLAG_ITEM_ROUTE_116_FIGHTING_GEM                            0x3E9 // Replaced X-Special
-#define FLAG_ITEM_ROUTE_104_PP_UP                                   0x3EA
+#define FLAG_ITEM_ROUTE_104_PP_UP                                   0x3EA // Replaced X-item
 #define FLAG_ITEM_ROUTE_105_IRON                                    0x3EB
 #define FLAG_ITEM_ROUTE_106_PROTEIN                                 0x3EC
 #define FLAG_ITEM_ROUTE_109_PP_UP                                   0x3ED
